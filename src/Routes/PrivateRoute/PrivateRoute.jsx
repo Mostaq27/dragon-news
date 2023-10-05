5f7cee9217d22762bb79cc7787393ebcddc2dc2e
Mostaq27 <mostaq99muzahid@gmail.com>
2023-10-05 13:48:21 +0600

    create private route and update auth provider

--- src/Routes/PrivateRoute/PrivateRoute.jsx
+++ src/Routes/PrivateRoute/PrivateRoute.jsx
@@ -1,10 +1,12 @@
 import { useContext } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 
 const PrivateRoute = ({children}) => {
     const {user, loading} = useContext(AuthContext);
+    const location = useLocation();
+    console.log(location);
 
     if(loading){
         return (<div className="flex justify-center items-center h-screen">
