ed77bcd5648a00b4d7b62d751de0130448162d47
Mostaq27 <mostaq99muzahid@gmail.com>
2023-10-06 15:19:53 +0600

    updated

--- src/Pages/Home/NewsCard.jsx
+++ src/Pages/Home/NewsCard.jsx
@@ -1,21 +1,36 @@
 import { Link } from "react-router-dom";
+import { FaRegBookmark, FaShareAlt } from "react-icons/fa";
+import moment from 'moment';
 
 
 const NewsCard = ({ news }) => {
-    const {title, image_url, details, _id} = news;
+    // console.log(news)
+    const { title, image_url, details, _id, author: { name, published_date, img }, } = news;
     return (
         <div className="card bg-base-100 shadow-xl mb-6">
+            <div className="">
+                <div className="flex pl-2">
+                    <figure className="h-[40px] rounded-full w-[40px]"><img src={img} alt="image" /></figure>
+                    <div className="px-2 flex-grow-1">
+                        <div className="">{name}</div>
+                        <div className=" text-sm">{moment(published_date).format('yyyy-MM-D')}</div>
+                    </div>
+                    <div className="flex items-center gap-2 ml-[300px] text-gray-200">
+                        <FaRegBookmark />  <FaShareAlt />
+                    </div>
+                </div>
+            </div>
             <figure className=" w-full"><img src={image_url} alt="image" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
-                 
-                    {
-                        details.length > 200 
+
+                {
+                    details.length > 200
                         ? <p>{details.slice(0, 200)} <Link to={`/news/${_id}`} className=" text-blue-700 font-bold">Read More...</Link></p>
                         : <p>{details}</p>
-                    }
-                 
-                 
+                }
+
+
             </div>
         </div>
     );
--- src/Pages/News/News.jsx
+++ src/Pages/News/News.jsx
@@ -1,11 +1,17 @@
-import { useParams } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
 import Header from "../Shared/Header/Header";
 import RightSideNav from "../Shared/Rightsidenav/RightSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
 
 
 const News = () => {
-    const {id} = useParams();
+
+    const news = useLoaderData();
+    console.log(news);
+
+    const { id, image_url, title } = useParams();
+    
+
     return (
         <div>
             <Header></Header>
@@ -15,6 +21,62 @@ const News = () => {
                     <h2 className="taxt-5xl">News details</h2>
                     <p>{id}</p>
 
+                    <div class="relative mt-6 flex  flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+                        <div class="p-6">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="currentColor"
+                                aria-hidden="true"
+                                class="mb-4 h-12 w-12 text-pink-500"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 01.75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 019.75 22.5a.75.75 0 01-.75-.75v-4.131A15.838 15.838 0 016.382 15H2.25a.75.75 0 01-.75-.75 6.75 6.75 0 017.815-6.666zM15 6.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"
+                                    clip-rule="evenodd"
+                                ></path>
+                                <path d="M5.26 17.242a.75.75 0 10-.897-1.203 5.243 5.243 0 00-2.05 5.022.75.75 0 00.625.627 5.243 5.243 0 005.022-2.051.75.75 0 10-1.202-.897 3.744 3.744 0 01-3.008 1.51c0-1.23.592-2.323 1.51-3.008z"></path>
+                            </svg>
+                            <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                                UI/UX Review Check
+                            </h5>
+                            <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                                Because it's about motivating the doers. Because I'm here to follow my
+                                dreams and inspire others.
+                            </p>
+                        </div>
+                        <div class="p-6 pt-0">
+                            <a
+                                class="!font-medium !text-blue-gray-900 !transition-colors hover:!text-pink-500"
+                                href="#"
+                            >
+                                <Link to="/">
+                                <button
+                                    class="flex select-none items-center gap-2 rounded-lg py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-pink-500 transition-all hover:bg-pink-500/10 active:bg-pink-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                                    type="button"
+                                    data-ripple-dark="true"
+                                >
+                                    Learn More
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke-width="2"
+                                        stroke="currentColor"
+                                        aria-hidden="true"
+                                        class="h-4 w-4"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                                        ></path>
+                                    </svg>
+                                </button>
+                                </Link>
+                            </a>
+                        </div>
+                    </div>
 
                 </div>
                 <div>
